34f02fa881d87161f3bfc1af9ac6ffaff82df396
Sarah <sarah.alhuriz@hotmail.com>
2021-03-19 18:52:49 -0600

    database collection ready

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -1,19 +1,6 @@
 const express = require('express')
 const router = express.Router();
-// const path = require('path');
 
-
-// var fs = require('fs');
-// var parse = require ('csv-parse');
-// const csvtojson = require('csvtojson');
-
-// ///Users/salhuriz/Projects/client-stats/server/gm_data.csv
-
-// csvtojson()
-//     .fromFile('../gm_data.csv')
-//     .then(csvData => {
-//         console.log(csvData);
-//     })
 
 const Customer = require('../models/customer.js');
 
